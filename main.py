5c1ab8c99137061ca0dce93a9534f04bc2e78fdd
Tharaka Wijekularatne <tharusnow@gmail.com>
2024-05-28 22:32:00 -0700

    env file read fix

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+from typing import Tuple
 import discord
 import os
+from discord import client
+from dotenv import load_dotenv
+from discord.ext import commands
+
+load_dotenv()
 
 intents = discord.Intents.default()
-
+# intents.message_content = True
 client = discord.Client(intents=intents)
-
+# client = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
 @client.event
 async def on_ready():
@@ -14,15 +20,18 @@ async def on_ready():
 @client.event
 async def on_message(message):
   if message.author == client.user:
+    print("Message from self")
     return
 
   if message.content.startswith('$hello'):
     await message.channel.send('Hello!')
 
 
-token = os.getenv("TOKEN")
+token = os.getenv("TOKEN2")
+print(token)
 if token is None:
   print(
-      "Error: Environment variable 'TOKEN' not found. Please set it   with your Discord bot token.")
+      "Error: Environment variable 'TOKEN' not found. Please set it   with your Discord bot token."
+  )
 else:
   client.run(token)
